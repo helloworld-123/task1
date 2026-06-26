88f74a3d27d164fb93e9ad66f95a637ce68d3c11
helloworld-123 <js1898337@gmail.com>
2026-06-26 10:50:52 +0530

    update the code

--- vote.c
+++ vote.c
@@ -1,13 +1,20 @@
 #include<stdio.h>
+
 int main(){
-    int age ;
-    printf("mention your age for verefication \n");
-    scanf("%d",&age);
-    if (age==18){
-        printf("u are aligable for vote");
-    }
-    else{
-        printf("u are not aligable for vote");
-    }
+    int age;
+    
+    do {
+        printf("Mention your age for verification: \n");
+        scanf("%d", &age);
+        
+        if (age < 18) {
+            printf("You are not eligible to vote. Please try again.\n\n");
+        }
+        
+    } while (age < 18); // Jab tak age 18 se kam hai, loop chalta rahega
+
+    // Jab user 18 ya usse bada input dega, loop khatam ho jayega aur yeh line chalegi
+    printf("You are eligible to vote!\n");
+    
     return 0;
 }
